4e2da9293ba952eb667a5a33ec9f529e7f28bdb2
AdamCotner <adamstevencotner@gmail.com>
2017-03-11 20:25:19 -0700

    script

---
+++ gitaddremovecommit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# gitaddremovecommit adds, removes, and commits all changes with a default message
+
+main() {	
+	# hints if you supply less or more than one argument
+	if [ $# -ne 1 ]
+		then
+			echo $#
+			print_usage
+			exit 1 
+	fi
+
+	# add all new, updated, and deleted files
+	git add .
+	git add -u
+
+	# commit
+	git commit -m "$1"
+}
+
+#usage information
+print_usage() {
+	echo "~~ gitaddremovecommit usage: ~~"
+	echo "- Please supply a commit message. If your message contains a <space>, please encase the whole message in double quotes."
+	echo "~~ ~~"
+}
+
+# go!
+main $1
