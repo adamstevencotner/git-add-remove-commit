5fbf08e748f6f55c59db9bb35c518c3a7e9721d4
AdamCotner <adamstevencotner@gmail.com>
2017-03-11 22:50:13 -0700

    -branch

--- gitaddremovecommit.sh
+++ gitaddremovecommit.sh
@@ -5,9 +5,20 @@ main() {
 	# hints if you supply less or more than one argument
 	if [ $# -eq 0 ]
 		then
-			echo $#
-			print_usage
+			print_general_usage
 			exit 1 
+	fi
+
+	if [ $1 -eq "-branch" ]
+		then
+			if [ $# -ne 2 ]
+				then
+					print_branch_usage
+					exit 1
+				else
+					# change the branch config
+					echo "$2"
+			fi		
 	fi
 
 	# add all new, updated, and deleted files
@@ -32,9 +43,15 @@ main() {
 }
 
 #usage information
-print_usage() {
+print_general_usage() {
 	echo "~~ gitaddremovecommit usage: ~~"
 	echo "- Please supply a commit message."
+	echo "~~ ~~"
+}
+
+print_branch_usage() {
+	echo "~~ gitaddremovecommit usage: ~~"
+	echo "- Please supply one branch name after '-branch'"
 	echo "~~ ~~"
 }
 
